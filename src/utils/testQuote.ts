9c78d779cbd33af7a7ae9994608a1300aa2e405d
johnc320 <132880523+johnc320@users.noreply.github.com>
2026-03-12 22:15:45 +0000

    Fix material metric property

--- src/utils/testQuote.ts
+++ src/utils/testQuote.ts
@@ -5,8 +5,16 @@ const total = calculateQuoteTotal(
   110,
   90,
   [
-    { material: { id: "1", name: "Power Point", cost: 12 }, quantity: 2, cost: 12 },
-    { material: { id: "2", name: "Cable", cost: 3 }, quantity: 10, cost: 3 }
+    {
+      material: { id: "1", name: "Power Point", cost: 12, metric: "each" },
+      quantity: 2,
+      cost: 12
+    },
+    {
+      material: { id: "2", name: "Cable", cost: 3, metric: "m" },
+      quantity: 10,
+      cost: 3
+    }
   ]
 )
 
